756c521c6e38904e672fe675047b495c87ef23fd
YeoP <kby7315@naver.com>
2025-10-24 16:36:49 +0900

    1차 파일 통합

--- FitAI/frontend/src/App.js
+++ FitAI/frontend/src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Community from "./pages/Community";
+import CommunityUserReview from "./components/CommunityUserPost";
 function App() {
-    return (_jsxs(Router, { children: [_jsx(Header, {}), _jsx(Routes, { children: _jsx(Route, { path: "/", element: _jsx(Home, {}) }) }), _jsx(Footer, {})] }));
+    return (_jsxs(Router, { children: [_jsx(Header, {}), _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/community", element: _jsx(Community, {}) }), _jsx(Route, { path: "/community/:postId", element: _jsx(CommunityUserReview, {}) })] }), _jsx(Footer, {})] }));
 }
 export default App;
--- FitAI/frontend/src/main.tsx
+++ FitAI/frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
-import Community from "./components/community";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
